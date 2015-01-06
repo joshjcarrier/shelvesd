04bcaf63b76b24f53608a3ff3891391c1035fbf5
Josh Carrier <joshjcarrier@gmail.com>
2015-01-06 06:11:29 +0000

    Bind to ipv4 only. Change port to 8123 when in production mode.

--- shelvesd.rb
+++ shelvesd.rb
@@ -1,5 +1,11 @@
 require 'sinatra'
 
+set :bind, '0.0.0.0'
+
+configure :production do
+  set :port, 8123
+end
+
 get '/', :provides => 'html' do
   '<html><body><a href=\'/api/v1/lights\'>/api/v1/lights</a><br/><a href=\'/api/v1/shelves\'>/api/v1/shelves</a></body></html>'
 end
